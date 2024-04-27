4604122991b1ab09f6582d61b6fe0d09d2ea85ac
prietzem <maxii.prietzel@gmail.com>
2024-04-27 10:10:41 +0200

    Config upadte

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,3 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 vim.keymap.set("n", "<space>t", require("toggleterm").toggle)
+
+-- Move to window using the <ctrl> hjkl keys
+-- map("n", "<leader><Left>", { desc = "Go to left window", remap = true })
+-- map("n", "<leader><Down>", { desc = "Go to lower window", remap = true })
+-- map("n", "<leader><Up>", { desc = "Go to upper window", remap = true })
+-- map("n", "<leader><Right>", { desc = "Go to right window", remap = true })
