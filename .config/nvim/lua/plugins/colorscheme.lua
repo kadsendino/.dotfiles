ca5f101a8e40e450182d82d49d14c61a88db9ad1
kadsendino <maxii.prietzel@gmail.com>
2024-05-23 13:58:36 +0200

    new colorscheme and keybinds

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,12 @@ return {
   -- add gruvbox
   { "pauchiner/pastelnight.nvim", name = "pastelnight", priority = 1000 },
   { "EdenEast/nightfox.nvim", name = "nightfox", priority = 1001 },
+  { "folke/tokyonight.nvim", name = "tokyonight", priority = 1002 },
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nightfox",
+      colorscheme = "tokyonight-night",
     },
   },
 }
