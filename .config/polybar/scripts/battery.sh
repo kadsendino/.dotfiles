e9b61abc18e8ee2822a4790dfa9d2f5971c16abf
prietzem <maxii.prietzel@gmail.com>
2025-07-07 18:36:15 +0200

    edited dotfiles

--- .config/polybar/scripts/battery.sh
+++ .config/polybar/scripts/battery.sh
@@ -5,7 +5,7 @@ status=$(cat /sys/class/power_supply/BAT0/status)
 
 if [[ "$status" == "Charging" ]]; then
   echo " $capacity%"
-elif [[ "$status" == "Discharging" ]]; then
+elif [[ "$status" == "Discharging" && $capacity -le 89 ]]; then
   echo " $capacity%"
 else
   echo " $capacity%"
