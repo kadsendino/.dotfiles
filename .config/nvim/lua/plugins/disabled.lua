99800f887bf7469227f16bb8f454af4adacd1d85
prietzem <maxii.prietzel@gmail.com>
2025-07-12 14:11:41 +0200

    edited neovim config

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,4 +1,8 @@
 return {
   -- disable noice
-  { "folke/noice.nvim", enabled = false },
+  { "folke/noice.nvim",           enabled = false },
+  { "lazy/markdown-preview.nvim", enabled = false },
+  { "render-markdown.nvim",      enabled = false },
+  { "mason-org/mason-lspconfig.nvim",  enabled = false },
+  { "lervag/vimtex" , enabled=false},
 }
